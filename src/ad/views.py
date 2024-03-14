e83cf8e7f5074a7972904130ca1a0e684226fb5f
farhadbaghban <baghbanfarhad@gmail.com>
2024-03-14 17:32:48 +0330

    fix(swagger) - fixed swagger configuration

--- src/ad/views.py
+++ src/ad/views.py
@@ -15,6 +15,23 @@ class CommentUpdateDeleteView(
     mixins.RetrieveModelMixin,
     GenericViewSet,
 ):
+    """
+    A ViewSet for handling CRUD operations on comments.
+
+    This ViewSet provides endpoints for creating, updating, retrieving, and deleting comments.
+
+    Attributes:
+        serializer_class (CommentSerializers): The serializer class used for serializing and deserializing comment data.
+        queryset (Comment.objects.all()): The queryset representing all comments.
+        permission_classes (IsOwnerOrReadOnly,): The permission classes applied to this ViewSet.
+
+    Methods:
+        create(request, *args, **kwargs): Handles HTTP POST requests for creating a new comment.
+        retrieve(request, *args, **kwargs): Handles HTTP GET requests for retrieving a comment by its ID.
+        update(request, *args, **kwargs): Handles HTTP PUT requests for updating an existing comment.
+        destroy(request, *args, **kwargs): Handles HTTP DELETE requests for deleting an existing comment.
+    """
+
     serializer_class = CommentSerializers
     queryset = Comment.objects.all()
 
@@ -24,6 +41,24 @@ class CommentUpdateDeleteView(
 
 
 class AdUpdateDeleteView(ModelViewSet):
+    """
+    A ViewSet for handling CRUD operations on Ads.
+
+    This ViewSet provides endpoints for creating, updating, retrieving, and deleting Ads.
+
+    Attributes:
+        serializer_class (AdSerializers): The serializer class used for serializing and deserializing Ad data.
+        queryset (Ad.objects.all()): The queryset representing all Ads.
+        permission_classes (IsOwnerOrReadOnly,): The permission classes applied to this ViewSet.
+
+    Methods:
+        create(request, *args, **kwargs): Handles HTTP POST requests for creating a new Ad.
+        retrieve(request, *args, **kwargs): Handles HTTP GET requests for retrieving a Ad by its ID.
+        list(request, *args, **kwargs): Handles HTTP GET requests for Listing Ads with another url.
+        update(request, *args, **kwargs): Handles HTTP PUT requests for updating an existing Ad.
+        destroy(request, *args, **kwargs): Handles HTTP DELETE requests for deleting an existing Ad.
+    """
+
     serializer_class = AdSerializers
     queryset = Ad.objects.all()
 
--- src/adcomment/urls.py
+++ src/adcomment/urls.py
@@ -17,15 +17,31 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+docsurlpatterns = [
+    # YOUR PATTERNS
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI:
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+]
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
-    path("ad/", include("ad.urls", namespace="ad")),
-    path(
-        "schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-]
+    path("adcommment/", include("ad.urls", namespace="ad")),
+] + docsurlpatterns
